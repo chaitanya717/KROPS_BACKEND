47d15ace447ebe85bff46d4443c6a60206c7ca92
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-10-23 15:40:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
@@ -12,12 +13,19 @@ connectDB();
 // Middleware
 app.use(bodyParser.json());
 
+// CORS configuration
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGINS, // Use ALLOWED_ORIGINS from .env or default to allowing all origins
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 // Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the Signup API !');
-  });
+  res.send('Welcome to the Signup API!');
+});
 app.use('/api/auth', authRoutes);
-
 
 // Server
 const PORT = process.env.PORT || 5000;

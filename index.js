ad27118f3558a5ade8ae877a383d56bc8f0e657d
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-10-23 16:15:17 +0530

    first

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
@@ -8,23 +7,29 @@ require('dotenv').config();
 const app = express();
 
 // Connect to MongoDB
-connectDB();
+connectDB().then(() => {
+  console.log('MongoDB connected successfully');
+}).catch((err) => {
+  console.error('Failed to connect to MongoDB:', err);
+  process.exit(1); // Exit the process if the database connection fails
+});
 
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json()); // Built-in body parser in Express
 
 // CORS configuration
-// const corsOptions = {
-//   origin: process.env.ALLOWED_ORIGINS, // Use ALLOWED_ORIGINS from .env or default to allowing all origins
-//   optionsSuccessStatus: 200, // For legacy browser support
-// };
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : '*',
+  optionsSuccessStatus: 200, // For legacy browser support
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the Signup API!');
 });
+
 app.use('/api/auth', authRoutes);
 
 // Server
